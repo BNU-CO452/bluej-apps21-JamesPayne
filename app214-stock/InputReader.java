d831f47e8cf5dcc96a4c3b249344a0300f948765
James Payne <79662097+JamesPayne24@users.noreply.github.com>
2021-12-01 14:11:07 +0000

    Final update

--- app214-stock/InputReader.java
+++ app214-stock/InputReader.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
  * InputReader reads typed text input from the standard text terminal. 
  * The text typed by a user is returned.
  * 
- * @author     Michael Kölling and David J. Barnes
- * @version    0.1 (2016.02.29)
+ * @author     James Payne
+ * @version    1
  * 
  * Modified by Derek Peacock 13/12/2020
  */
